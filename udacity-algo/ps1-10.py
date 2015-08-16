a8cad3a9f496025a67f8d6a4409ff4721d0f4efe
henryfw <henryfw@gmail.com>
2015-08-15 19:32:09 -0700

    a

--- udacity-algo/ps1-10.py
+++ udacity-algo/ps1-10.py
@@ -11,7 +11,7 @@
 
 
 # global vars
-nodes = dict() # key is node id, value is list of connected nodes
+globalNodes = dict() # key is node id, value is list of connected nodes
 
 
 def getEdgeId(x, y):
@@ -19,30 +19,26 @@ def getEdgeId(x, y):
 
 # try dfs
 def dfs(path, outgoingNodes, usedEdges, edgeTotal):
+    # print path
 
-    # ending condition in last cycle
-    if len(usedEdges) == edgeTotal - 1:
-        startingNode = path[0]
-        if startingNode in outgoingNodes:
-            edgeId = getEdgeId(startingNode, path[-1])
-            if edgeId not in usedEdges:
-                # found it
-                path.append(startingNode)
+    for node in outgoingNodes:
+        edgeId = getEdgeId(node, path[-1])
+        if edgeId not in usedEdges:
+            # ending condition in last cycle
+            if len(usedEdges) == edgeTotal - 1:
+                path.append(node)
                 usedEdges.append(edgeId)
                 return path
-        return None
-
-    else:
-        for node in outgoingNodes:
-            edgeId = getEdgeId(node, path[-1])
-            if edgeId not in usedEdges:
-                usedEdges.append(edgeId)
-                path.append(node)
-                # print path
-                result = dfs(path, nodes[node], usedEdges, edgeTotal)
+            else:
+                tmpUsedEdges = usedEdges[:]
+                tmpUsedEdges.append(edgeId)
+                tmpPath = path[:]
+                tmpPath.append(node)
+                result = dfs(tmpPath, globalNodes[node], tmpUsedEdges, edgeTotal)
                 if result is not None:
                     return result
-        return None
+
+    return None
 
 
 
@@ -54,24 +50,24 @@ def find_eulerian_tour(graph):
         fromNode, toNode = edge
 
         # save all outgoing edges from each node. this is undirected, so do 2 ways
-        if fromNode not in nodes:
-            nodes[fromNode] = list()
-        if toNode not in nodes:
-            nodes[toNode] = list()
+        if fromNode not in globalNodes:
+            globalNodes[fromNode] = list()
+        if toNode not in globalNodes:
+            globalNodes[toNode] = list()
 
-        if toNode not in nodes[fromNode]:
-            nodes[fromNode].append(toNode)
-        if fromNode not in nodes[toNode]:
-            nodes[toNode].append(fromNode)
+        if toNode not in globalNodes[fromNode]:
+            globalNodes[fromNode].append(toNode)
+        if fromNode not in globalNodes[toNode]:
+            globalNodes[toNode].append(fromNode)
 
         # keep a count of total edges
         edgeTotal += 1
 
-
+    #print nodes
 
     # try all node in nodes as starting node in order
-    for startingNode in nodes:
-        outgoingEdges = nodes[startingNode]
+    for startingNode in globalNodes:
+        outgoingEdges = globalNodes[startingNode]
 
         path = list()
         path.append(startingNode)
@@ -88,7 +84,18 @@ inputs = [(0, 1), (1, 5), (1, 7), (4, 5),
           (4, 8), (1, 6), (3, 7), (5, 9),
           (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 
-inputs = [(1, 2), (2, 3), (3, 1), (3, 4), (4, 3)]
+inputs = [(1, 2), (2, 3), (3, 1), (3, 4)]
+inputs = [(2, 6), (4, 2), (5, 4), (6, 5), (6, 8), (7, 9), (8, 7), (9, 6)]
 
 print find_eulerian_tour(inputs)
+
+# tmp [0, 1, 6, 3, 7, 1, 5, 9, 8, 4, 5, 2, 4, 0]
+#     [0, 1, 7, 3, 6, 1, 5, 4, 8, 9, 5, 2, 4, 0]
+
+
 # print edges
+
+
+
+
+
