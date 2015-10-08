083e7cb6c763227098dcb2f041d0b702f526bfd1
henryfw <uprz23@gmail.com>
2015-10-07 20:31:56 -0700

    a

--- aziz/php/ch05/pascals-triangle.php
+++ aziz/php/ch05/pascals-triangle.php
@@ -1,5 +1,19 @@
 <?php
 
 function getFirstRowsPascalsTriangle($n) {
+    $ans = [];
 
+    for ($i = 0; $i < $n; $i ++ ){
+        $row = [];
+        for ($j = 0; $j <= $i; $j ++) {
+            if (0 < $j && $j < $i) {
+                $row[] = $ans[count($ans) - 1][$j - 1] + $ans[count($ans) - 1][$j];
+            }
+            else $row[] = 1;
+        }
+        $ans[] = $row;
+    }
+    return $ans;
 }
+
+print_r(getFirstRowsPascalsTriangle(10));
--- aziz/python/ch05/long-precision-multiply.py
+++ aziz/python/ch05/long-precision-multiply.py
@@ -1,4 +1,32 @@
 
 
 def longPrecisionMultiply(a, b):
-    pass
+    neg = (a[0] < 0 and b[0] >= 0) or (a[0] >= 0 and b[0] < 0)
+
+    a[0] = abs(a[0])
+    b[0] = abs(b[0])
+
+    a.reverse()
+    b.reverse()
+
+    result = [0 for i in range(len(a) + len(b) + 2)]
+
+    for i in range(0, len(a)):
+        for j in range(0, len(b)):
+            result[i + j] += a[i] * b[j]
+            result[i + j + 1] +=  int( result[i + j] / 10 )
+            result[i + j] %= 10
+
+    while len(result) != 1 and result[-1] == 0:
+        result.pop()
+
+    result.reverse()
+
+    if neg:
+        result *= -1
+
+    return result
+
+
+
+print longPrecisionMultiply([2,5], [1,0])
