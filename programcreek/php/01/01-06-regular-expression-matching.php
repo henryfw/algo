072121e5a590db624a9e4572dd2e99922c7727ff
henryfw <uprz23@gmail.com>
2015-12-21 00:49:24 -0800

    a

--- programcreek/php/01/01-06-regular-expression-matching.php
+++ programcreek/php/01/01-06-regular-expression-matching.php
@@ -6,6 +6,7 @@
  */
 
 function isMatch($str, $pattern) {
+    echo "checking $str for $pattern\n";
     if (strlen($pattern) == 0) {
         return strlen($str) == 0;
     }
@@ -27,7 +28,6 @@ function isMatch($str, $pattern) {
 
         $i = 0;
         while ($i < strlen($str) && ($str{$i} == $pattern{0}) || $pattern{0} == '.') {
-
             if (isMatch(substr($str, $i + 1), substr($pattern, 2))) return true;
 
             $i++;
@@ -39,12 +39,13 @@ function isMatch($str, $pattern) {
 }
 
 
-$ans[] = isMatch("aa","a") ;
-$ans[] = isMatch("aa","aa") ;
-$ans[] = isMatch("aaa","aa") ;
-$ans[] = isMatch("aa", "a*") ;
-$ans[] = isMatch("aa", ".*") ;
-$ans[] = isMatch("ab", ".*") ;
-$ans[] = isMatch("aab", "c*a*b") ;
+//$ans[] = isMatch("aa","a") ;
+$ans[] = isMatch("aba","aca") ;
+//$ans[] = isMatch("aa","aa") ;
+//$ans[] = isMatch("aaa","aa") ;
+//$ans[] = isMatch("aa", "a*") ;
+//$ans[] = isMatch("aa", ".*") ;
+//$ans[] = isMatch("ab", ".*") ;
+//$ans[] = isMatch("aab", "c*a*b") ;
 
 print_r($ans);
